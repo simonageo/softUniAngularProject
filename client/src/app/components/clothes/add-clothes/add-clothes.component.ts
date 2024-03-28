3681e3b39bb469fc35c333ad656f06d35e5b3bcd
Simona Georgieva <sag954@gmail.com>
2024-03-28 18:52:39 +0200

    add-clothing service

--- client/src/app/components/clothes/add-clothes/add-clothes.component.ts
+++ client/src/app/components/clothes/add-clothes/add-clothes.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ClothesService } from '../clothes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-clothes',
@@ -7,7 +9,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./add-clothes.component.css']
 })
 export class AddClothesComponent {
-  constructor( ){}
+  constructor(private clothesService: ClothesService, private router: Router){}
   validatePrice(form: NgForm) {
     const priceControl = form.controls['price'];
     if(priceControl.value===null){
@@ -15,10 +17,19 @@ export class AddClothesComponent {
     } else if (isNaN(priceControl.value) || priceControl.value < 0) {
       priceControl.setErrors({ 'invalidPrice': true });
     } else {
-      priceControl.setErrors({ 'invalidPrice': false });
-      priceControl.setErrors({ 'required': false });
+      priceControl.setErrors(null);
     }
     priceControl.markAsTouched();
   }
   
+  addItem(form: NgForm){
+    if (form.invalid) {
+      console.log('invalid form')
+      return;
+    }
+    const {title, category, imageUrl, price, description}=form.value;
+    this.clothesService.addClothing(title, category, imageUrl, price, description).subscribe((res)=>{
+      this.router.navigate(['/'])
+    })
+  }
 }
