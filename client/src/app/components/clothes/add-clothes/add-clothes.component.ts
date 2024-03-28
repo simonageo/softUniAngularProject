e297c9b265193260305d82d0704add9419330312
Simona Georgieva <sag954@gmail.com>
2024-03-28 16:02:28 +0200

    adding some validations for add-clothes

--- client/src/app/components/clothes/add-clothes/add-clothes.component.ts
+++ client/src/app/components/clothes/add-clothes/add-clothes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-clothes',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-clothes.component.css']
 })
 export class AddClothesComponent {
-
+  constructor( ){}
+  validatePrice(form: NgForm) {
+    const priceControl = form.controls['price'];
+    if(priceControl.value===null){
+      priceControl.setErrors({ 'required': true });
+    } else if (isNaN(priceControl.value) || priceControl.value < 0) {
+      priceControl.setErrors({ 'invalidPrice': true });
+    } else {
+      priceControl.setErrors({ 'invalidPrice': false });
+      priceControl.setErrors({ 'required': false });
+    }
+    priceControl.markAsTouched();
+  }
+  
 }
