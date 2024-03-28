ff1816d574a0befcc9ce7b368fa80787ef4284ce
Simona Georgieva <sag954@gmail.com>
2024-03-28 23:18:38 +0200

    adding some css to the store

--- client/src/app/components/clothes/all-clothes/all-clothes.component.ts
+++ client/src/app/components/clothes/all-clothes/all-clothes.component.ts
@@ -9,10 +9,27 @@ import { ClothesService } from '../clothes.service';
 })
 export class AllClothesComponent implements OnInit {
   clothes: Clothing[] | null = [];
+  clothesRows: Clothing[][] = []; // Define clothesRows array
+
   constructor(private clothesService: ClothesService) {}
+
   ngOnInit(): void {
-    this.clothesService
-      .getAllClothes()
-      .subscribe((data: Clothing[]) => {this.clothes = data});
+    this.clothesService.getAllClothes().subscribe((data: Clothing[]) => {
+      this.clothes = data;
+      this.organizeClothesIntoRows();
+    });
+  }
+
+  organizeClothesIntoRows(): void {
+    const itemsPerRow = 3;
+    let currentRow: Clothing[] = [];
+
+    this.clothes?.forEach((item, index) => {
+      currentRow.push(item);
+      if ((index + 1) % itemsPerRow === 0 || index === this.clothes!.length - 1) {
+        this.clothesRows.push(currentRow);
+        currentRow = [];
+      }
+    });
   }
 }
