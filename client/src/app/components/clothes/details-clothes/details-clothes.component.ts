5f66ae4f0592be15a4b977846a9bc3761f62f889
Simona Georgieva <sag954@gmail.com>
2024-04-01 14:02:18 +0300

    adding ondestroy

--- client/src/app/components/clothes/details-clothes/details-clothes.component.ts
+++ client/src/app/components/clothes/details-clothes/details-clothes.component.ts
@@ -1,30 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ClothesService } from '../clothes.service';
 import { Clothing } from 'src/app/types/clothes';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-details-clothes',
   templateUrl: './details-clothes.component.html',
   styleUrls: ['./details-clothes.component.css'],
 })
-export class DetailsClothesComponent implements OnInit {
+export class DetailsClothesComponent implements OnInit, OnDestroy {
   itemId: string = '';
   clothing: Clothing | null = null;
   ownerId: string ='';
+  paramsSubscription: Subscription | null = null;
+  clothingSubscription: Subscription | null = null;
   constructor(
     private route: ActivatedRoute,
     private clothesService: ClothesService
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
+    this.paramsSubscription = this.route.params.subscribe((params) => {
       this.itemId = params['id'];
     });
-    this.clothesService.getOneClothing(this.itemId).subscribe(
+    this.clothingSubscription = this.clothesService.getOneClothing(this.itemId).subscribe(
       (data) => {
         this.clothing = data;
-        this.ownerId=data['_ownerId'];
+        this.ownerId = data['_ownerId'];
       }
     );
   }
@@ -36,5 +39,10 @@ export class DetailsClothesComponent implements OnInit {
   isOwner(): boolean {
     const userId=localStorage.getItem('userId');
     return userId===this.ownerId;
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription?.unsubscribe();
+    this.clothingSubscription?.unsubscribe();
   }
 }
--- client/src/app/components/clothes/edit-clothing/edit-clothing.component.ts
+++ client/src/app/components/clothes/edit-clothing/edit-clothing.component.ts
@@ -1,27 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ClothesService } from '../clothes.service';
 import { Clothing } from 'src/app/types/clothes';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-clothing',
   templateUrl: './edit-clothing.component.html',
   styleUrls: ['./edit-clothing.component.css']
 })
-export class EditClothingComponent implements OnInit{
+export class EditClothingComponent implements OnInit, OnDestroy{
   itemId: string = '';
   clothing: Clothing | null = null;
+  paramsSubscription: Subscription | null = null;
+  clothingSubscription: Subscription | null = null;
 
   constructor (private route: ActivatedRoute, private clothesService: ClothesService){}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params)=>{
-      this.itemId=params['id']
+    this.paramsSubscription = this.route.params.subscribe(params => {
+      this.itemId = params['id'];
     });
-    this.clothesService.getOneClothing(this.itemId).subscribe(data=>{
-      this.clothing=data;
-    })
+    this.clothingSubscription = this.clothesService.getOneClothing(this.itemId).subscribe(data => {
+      this.clothing = data;
+    });
   }
   editItem(form: NgForm){
     //
@@ -37,5 +40,10 @@ export class EditClothingComponent implements OnInit{
       priceControl.setErrors(null);
     }
     priceControl.markAsTouched();
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription?.unsubscribe();
+    this.clothingSubscription?.unsubscribe();
   }
 }
